4823b80644debd898c6b7162b0e97b16d720cb36
CaelMcDermott <49283495+CaelMcDermott@users.noreply.github.com>
2019-11-20 10:42:45 -0800

    Final Expo Assignment for Fall Tri
    Code for my Expo Assignment.

---
+++ App.js
@@ -0,0 +1,65 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import Constants from 'expo-constants';
+
+// You can import from local files
+import AssetExample from './components/AssetExample';
+
+// or any pure javascript modules available in npm
+import { Card } from 'react-native-paper';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: "space-around",
+          height: 150,
+          padding: 20,
+        }}>
+        <View style={{ backgroundColor: 'blue', flex: 5 }} />
+        <View style={{ backgroundColor: 'red', flex: 5 }} >
+        <Button
+  onPress={() => {
+    alert ('Move forward, robot!');
+    fetch('https://bot3.seattleacademy.software/drive?left=100&right=100')
+  }}
+  title="Press Me"
+/>
+        </View>
+        
+        <Text>Hello World!</Text>
+        
+      </View>
+
+
+      
+    
+        <Text style={styles.paragraph}>
+          Change code in the editor and watch it change on your phone!!! Save to get a shareable url.
+        </Text>
+        <Card>
+          <AssetExample />
+        </Card>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: .8,
+    justifyContent: 'space-around',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
